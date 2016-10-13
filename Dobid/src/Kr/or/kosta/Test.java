0ad2e7dae262f84054d262f7d0c25a34f7e295b4
admin <admin@192.168.0.112>
2016-10-13 16:09:52 +0900

    Test.java

--- Dobid/src/Kr/or/kosta/Test.java
+++ Dobid/src/Kr/or/kosta/Test.java
@@ -2,7 +2,11 @@ package Kr.or.kosta;
 
 public class Test {
 	public static void main(String[] args) {
-		String choifeel="¹Ùº¸";
+		String choifeel = "¹Ùº¸";
 		System.out.println(choifeel);
+		String parkjihye = "ºéµü";
+		while (true) {
+			System.out.println(parkjihye);
+		}
 	}
 }
