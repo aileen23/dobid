4fd2f8e89fafe188389527165954f69758df5c58
admin <admin@192.168.0.112>
2016-10-31 16:27:05 +0900

    123

--- Dobid/src/com/dobid/actions/mypage/Mypage_withdrawal_Action.java
+++ Dobid/src/com/dobid/actions/mypage/Mypage_withdrawal_Action.java
@@ -1,7 +1,5 @@
 package com.dobid.actions.mypage;
 
-import java.util.List;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -10,22 +8,20 @@ import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 
-import com.dobid.beans.Auction_myAuctionDTO;
 import com.dobid.model.Mypage_DAO;
 
-public class Mypage_withdrawal_Action extends Action{
+public class Mypage_withdrawal_Action extends Action {
 	@Override
 	public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request,
 			HttpServletResponse response) throws Exception {
-		
-		String id= (String) request.getSession().getAttribute("logincheck");
-		
+
+		String id = (String) request.getSession().getAttribute("logincheck");
+
 		Mypage_DAO dao = new Mypage_DAO();
-		
-		
+
 		dao.deleteid(id);
-		
-		return  mapping.findForward("success");
+		return mapping.findForward("success");
+
 	}
 
 }
