6f95edaea5efeee33ed83d68a3666b7d172a93fa
이종환 <@192.168.0.192>
2016-11-02 17:54:17 +0900

    본인 댓글만 삭제 가능

--- Dobid/src/com/dobid/actions/reply/notice/DeleteAction.java
+++ Dobid/src/com/dobid/actions/reply/notice/DeleteAction.java
@@ -8,6 +8,7 @@ import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 
+import com.dobid.beans.Reply_noticeDTO;
 import com.dobid.model.ReplyDAO;
 
 
@@ -18,14 +19,24 @@ public class DeleteAction extends Action{
 			HttpServletResponse response) throws Exception {
 	   request.setCharacterEncoding("UTF-8");
 		int no = Integer.parseInt(request.getParameter("no"));
+		String name = request.getParameter("name");
+		System.out.println(no);
+		System.out.println(name);
+		
+		if(name.equals("")){
+			request.setAttribute("msg", "로그인이 필요합니다");
+		}else{
+			Reply_noticeDTO reply_notice = new Reply_noticeDTO();
+			reply_notice.setNo(no);
+			reply_notice.setName(name);
 		
 		ReplyDAO dao = new ReplyDAO();
-		   if(dao.delete_notice(no)){
+		   if(dao.delete_notice(reply_notice)){
 			   request.setAttribute("msg", "댓글삭제성공!!");
 		   }else{
-			   request.setAttribute("msg", "댓글삭제실패!!"); 
+			   request.setAttribute("msg", "본인의 댓글만 삭제 가능합니다"); 
 		   }
-		   
+		}   
 		   
 		return mapping.findForward("success");
 	}
