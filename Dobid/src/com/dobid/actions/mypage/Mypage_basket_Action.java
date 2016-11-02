4288dca544fceed6adf50f47c0e81cef23a92932
kosta <kosta@192.168.0.142>
2016-11-02 15:59:10 +0900

    111

--- Dobid/src/com/dobid/actions/mypage/Mypage_basket_Action.java
+++ Dobid/src/com/dobid/actions/mypage/Mypage_basket_Action.java
@@ -18,15 +18,27 @@ public class Mypage_basket_Action extends Action{
 	@Override
 	public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request,
 			HttpServletResponse response) throws Exception {
-		
 		String id= (String) request.getSession().getAttribute("logincheck");
-		
+		String action = request.getParameter("action");
+		String pageStr = request.getParameter("page");
+		int page=1;
+		int viewRowCnt=10;
+  	  if(pageStr != null){
+		  page = Integer.parseInt(pageStr);
+	  }
+	  int end=page*viewRowCnt;
+	  int start=end-(viewRowCnt-1);
+	  
 		Mypage_DAO dao = new Mypage_DAO();
-		
-		List<AuctionDTO> list = dao.basketSelect(id);
+		int totalRecord=dao.countlist(id);
+		int totalPage = totalRecord/viewRowCnt;
+	      if(totalRecord%viewRowCnt >0)
+    	  totalPage++;
+		List<AuctionDTO> list = dao.basketSelect(id,start,end);
 		
 		request.setAttribute("list", list);
-		
+		request.setAttribute("page", page);//현재페이지
+  	  	request.setAttribute("totalPage", totalPage);//전체페이지
 		return  mapping.findForward("success");
 	}
 }
